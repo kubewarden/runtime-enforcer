2facb1e8944cab92e3dd95f6140cfc786eab8fec
Alessio Biancalana <alessio.biancalana@suse.com>
2026-09-04 14:55:28 +0200

    fix: api doc annotation from runtime-enforcer api group to runtimeenforcer
    Signed-off-by: Alessio Biancalana <alessio.biancalana@suse.com>

--- api/v1alpha1/doc.go
+++ api/v1alpha1/doc.go
@@ -1,6 +1,6 @@
 // +k8s:deepcopy-gen=package
 // +k8s:openapi-gen=true
-// +k8s:openapi-model-package=com.github.kubewarden.runtime-enforcer.api.v1alpha1
+// +k8s:openapi-model-package=com.github.kubewarden.runtimeenforcer.api.v1alpha1
 // +groupGoName=RuntimeEnforcer
 // +groupName=runtimeenforcer.kubewarden.io
 
